4b1c42bd20055aeef2dc42e6dbbaf3e0669a6544
JessicaCoelho21 <jscoelho21@gmail.com>
2021-05-07 15:24:45 +0100

    NLW5 ReactJS Aula 1

---
+++ React/NLW5_ReactJS/podcastr/src/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Button(props) {
+  const [counter, setCounter] = React.useState(1);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <>
+      <span>{counter}</span>
+      <button onClick={increment}>{props.children}</button>
+      <br />
+    </>
+  );
+}
